81b6288773ae7f0b3ef405e278dc001c55badfcd
╰‿╯MR HANSAMALAད★࿐ <127263909+mrhansamala@users.noreply.github.com>
2024-07-05 10:04:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import {
-    makeWASocket,
-    Browsers,
-    jidDecode,
-    makeInMemoryStore,
-    makeCacheableSignalKeyStore,
-    fetchLatestBaileysVersion,
-    DisconnectReason,
-    useMultiFileAuthState,
-    getAggregateVotesInPollMessage
-} from '@whiskeysockets/baileys';
+import { makeWASocket, Browsers, jidDecode, makeInMemoryStore, makeCacheableSignalKeyStore, fetchLatestBaileysVersion, DisconnectReason, useMultiFileAuthState, getAggregateVotesInPollMessage } from '@whiskeysockets/baileys';
 import { Handler, Callupdate, GroupUpdate } from './event/index.js';
 import { Boom } from '@hapi/boom';
 import express from 'express';
@@ -24,10 +14,9 @@ import moment from 'moment-timezone';
 import axios from 'axios';
 import fetch from 'node-fetch';
 import * as os from 'os';
-import config from '../config.cjs';
+import config from '../config.cjs';  
 import pkg from '../lib/autoreact.cjs';
 const { emojis, doReact } = pkg;
-
 
 const sessionName = "session";
 const app = express();
@@ -53,105 +42,107 @@ const store = makeInMemoryStore({
     })
 });
 
+// Baileys Connection Option
 async function start() {
-    try {
-        if (!config.SESSION_ID) {
-            useQR = true;
-            isSessionPutted = false;
-        } else {
-            useQR = false;
-            isSessionPutted = true;
-        }
+    if (!config.SESSION_ID) {
+        useQR = true;
+        isSessionPutted = false;
+    } else {
+        useQR = false;
+        isSessionPutted = true;
+    }
 
-        let { state, saveCreds } = await useMultiFileAuthState(sessionName);
-        let { version, isLatest } = await fetchLatestBaileysVersion();
-        console.log(chalk.red("CODED BY MR HANSAMALA @94781708673"));
-        console.log(chalk.green(`using WA v${version.join(".")}, isLatest: ${isLatest}`));
+    let { state, saveCreds } = await useMultiFileAuthState(sessionName);
+    let { version, isLatest } = await fetchLatestBaileysVersion();
+    console.log(chalk.red("CODED BY MR HANSAMALA @94781708673"));
+    console.log(chalk.green(`using WA v${version.join(".")}, isLatest: ${isLatest}`));
 
-        const Device = (os.platform() === 'win32') ? 'Windows' : (os.platform() === 'darwin') ? 'MacOS' : 'Linux';
-        const Matrix = makeWASocket({
-            version,
-            logger: pino({ level: 'silent' }),
-            printQRInTerminal: useQR,
-            browser: [Device, 'chrome', '121.0.6167.159'],
-            patchMessageBeforeSending: (message) => {
-                const requiresPatch = !!(
-                    message.buttonsMessage ||
-                    message.templateMessage ||
-                    message.listMessage
-                );
-                if (requiresPatch) {
-                    message = {
-                        viewOnceMessage: {
-                            message: {
-                                messageContextInfo: {
-                                    deviceListMetadataVersion: 2,
-                                    deviceListMetadata: {},
-                                },
-                                ...message,
+    const Device = (os.platform() === 'win32') ? 'Windows' : (os.platform() === 'darwin') ? 'MacOS' : 'Linux';
+    const Matrix = makeWASocket({
+        version,
+        logger: pino({ level: 'silent' }),
+        printQRInTerminal: useQR,
+        browser: [Device, 'chrome', '121.0.6167.159'],
+        patchMessageBeforeSending: (message) => {
+            const requiresPatch = !!(
+                message.buttonsMessage ||
+                message.templateMessage ||
+                message.listMessage
+            );
+            if (requiresPatch) {
+                message = {
+                    viewOnceMessage: {
+                        message: {
+                            messageContextInfo: {
+                                deviceListMetadataVersion: 2,
+                                deviceListMetadata: {},
                             },
+                            ...message,
                         },
-                    };
-                }
-                return message;
-            },
-            auth: {
-                creds: state.creds,
-                keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
-            },
-            getMessage: async (key) => {
-                if (store) {
-                    const msg = await store.loadMessage(key.remoteJid, key.id);
-                    return msg.message || undefined;
-                }
-                return {
-                    conversation: "Hello World"
+                    },
                 };
-            },
-            markOnlineOnConnect: true,
-            generateHighQualityLinkPreview: true,
-            defaultQueryTimeoutMs: undefined,
-            msgRetryCounterCache
-        });
-        store?.bind(Matrix.ev);
-
-        if (!Matrix.authState.creds.registered && isSessionPutted) {
-            const sessionID = config.SESSION_ID.split('Red_Fox-MD:/')[1];
-            const pasteUrl = `https://pastebin.com/raw/${sessionID}`;
-            const response = await fetch(pasteUrl);
-            const text = await response.text();
-            if (typeof text === 'string') {
-                if (!fs.existsSync('../session/creds.json')) {
-                    fs.writeFileSync('../session/creds.json', text);
-                    console.log('session file created');
-                    await start();
-                } else {
-                    console.log('session file already exists');
-                }
             }
-        }
-
-        async function getMessage(key) {
+            return message;
+        },
+        auth: {
+            creds: state.creds,
+            keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+        },
+        getMessage: async (key) => {
             if (store) {
                 const msg = await store.loadMessage(key.remoteJid, key.id);
-                return msg?.message;
+                return msg.message || undefined;
             }
             return {
-                conversation: "Hello World",
+                conversation: "Hello World"
             };
+        },
+        markOnlineOnConnect: true,
+        generateHighQualityLinkPreview: true,
+        defaultQueryTimeoutMs: undefined,
+        msgRetryCounterCache
+    });
+    store?.bind(Matrix.ev);
+
+    // Manage Device Logging
+    if (!Matrix.authState.creds.registered && isSessionPutted) {
+        const sessionID = config.SESSION_ID.split('Red_Fox-MD:/')[1];
+        const pasteUrl = `https://pastebin.com/raw/${sessionID}`;
+        const response = await fetch(pasteUrl);
+        const text = await response.text();
+        if (typeof text === 'string') {
+            fs.writeFileSync('../session/creds.json', text);
+            console.log('session file created');
+            await start();
         }
+    }
 
-        Matrix.ev.on("messages.upsert", async chatUpdate => await Handler(chatUpdate, Matrix, logger));
-        Matrix.ev.on("call", async (json) => await Callupdate(json, Matrix));
-        Matrix.ev.on("group-participants.update", async (messag) => await GroupUpdate(Matrix, messag));
-
-        if (config.MODE === "public") {
-            Matrix.public = true;
-        } else if (config.MODE === "private") {
-            Matrix.public = false;
+    // Response cmd pollMessage
+    async function getMessage(key) {
+        if (store) {
+            const msg = await store.loadMessage(key.remoteJid, key.id);
+            return msg?.message;
         }
+        return {
+            conversation: "Hello World",
+        };
+    }
 
-        Matrix.ev.on("connection.update", async update => {
+    // Handle Incomming Messages
+    Matrix.ev.on("messages.upsert", async chatUpdate => await Handler(chatUpdate, Matrix, logger));
+    Matrix.ev.on("call", async (json) => await Callupdate(json, Matrix));
+    Matrix.ev.on("group-participants.update", async (messag) => await GroupUpdate(Matrix, messag));
+
+    // Setting public or self mode based on config
+    if (config.MODE === 'public') {
+        Matrix.public = true;
+    } else if (config.MODE === 'self') {
+        Matrix.public = false;
+    }
+
+
+    // Check Baileys connections
+  Matrix.ev.on("connection.update", async update => {
             const { connection, lastDisconnect } = update;
             if (connection === "close") {
                 let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
@@ -174,8 +165,9 @@ async function start() {
                     console.error("❌ Something Went Wrong: Failed to Make Connection❌", reason);
                 }
             }
+    }
 
-            if (connection === "open") {
+       if (connection === "open") {
                 if (initialConnection) {
                     console.log(chalk.green("Integration Successful️ ✅"));
                     Matrix.sendMessage(Matrix.user.id, { text: `*🦊ʀᴇᴅ-ꜰᴏx-ᴍᴅ ᴅᴇᴠᴏʟᴏᴘɪɴɢ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟ* 
@@ -189,32 +181,26 @@ async function start() {
             }
         });
 
-        Matrix.ev.on('messages.upsert', async chatUpdate => {
-            try {
-                const mek = chatUpdate.messages[0];
-                if (!mek.key.fromMe && config.AUTO_REACT) {
-                    console.log(mek);
-                    if (mek.message) {
-                        const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-                        await doReact(randomEmoji, mek, Matrix);
-                    }
-                }
-            } catch (err) {
-                console.error('Error during auto reaction:', err);
-            }
-        });
-    } catch (error) {
-        console.error('Critical Error:', error);
-        process.exit(1);
+Matrix.ev.on('messages.upsert', async chatUpdate => {
+  try {
+    const mek = chatUpdate.messages[0];
+    if (!mek.key.fromMe && config.AUTO_REACT) {
+      console.log(mek);
+      if (mek.message) {
+        const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+        await doReact(randomEmoji, mek, Matrix);
+      }
     }
+  } catch (err) {
+    console.error('Error during auto reaction:', err);
+  }
+});
 }
 
 start();
-
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
