1ece9be6bc211c08e6c8c080c9b64407fe8a2102
╰‿╯MR HANSAMALAད★࿐ <127263909+mrhansamala@users.noreply.github.com>
2024-07-19 07:41:51 -0700

    Update play.js

--- plugin/play.js
+++ plugin/play.js
@@ -1,4 +1,4 @@
-import ytdl from 'ytdl-core';
+import ytdl from '@distube/ytdl-core';
 import ytSearch from 'yt-search';
 import pkg, { prepareWAMessageMedia } from '@whiskeysockets/baileys';
 const { generateWAMessageFromContent, proto } = pkg;
