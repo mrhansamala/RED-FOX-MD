a2d772ece800c4eb842cb908b7ef1f494e02e228
╰‿╯MR HANSAMALAད★࿐ <127263909+mrhansamala@users.noreply.github.com>
2024-07-19 07:39:50 -0700

    FIX

--- plugin/ytv.js
+++ plugin/ytv.js
@@ -1,4 +1,4 @@
-import ytdl from 'ytdl-core';
+import ytdl from '@distube/ytdl-core';
 import pkg, { prepareWAMessageMedia } from '@whiskeysockets/baileys';
 const { generateWAMessageFromContent, proto } = pkg;
 
