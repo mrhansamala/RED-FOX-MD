66668ca5350856def639c6e832e89576da7c1e98
Numberfake794 <154753115+Numberfake794@users.noreply.github.com>
2024-08-09 08:18:07 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -10,16 +10,16 @@ const config = {
   AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : false,
   AUTO_RECORDING: process.env.AUTO_RECORDING !== undefined ? process.env.AUTO_RECORDING === 'true' : false,
   ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== undefined ? process.env.ALWAYS_ONLINE === 'true' : false,
-  AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : false,
+  AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : true,
    /*auto block only for 212 */
-  AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : true,
+  AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : false,
   
   
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "🧛⛬ᴍʀ-ʜᴀɴꜱᴀᴍᴀʟᴀ⛬🦊",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "94781708673",
+  OWNER_NAME: process.env.OWNER_NAME || "🅢𝐇𝐀𝐍𝐈 🅜𝐃⁹⁹⁴",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "923455116994",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyC-IssuZUK4AnHJ1Johrwoe3Od_PxoOaPI",
   BING_API_KEY: process.env.BING_API_KEY || "818b3495f3fe4c54b5e4bb514b864807",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
